ee6b01a9d81399808ea9b32cbd13c9cd4c1061e8
maol07001 <maol07001@E-PW05CGR4>
2024-11-14 15:44:54 +0100

    Add project files.

---
+++ uppgift4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace uppgift_3._4
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Låtens längd i minuter och sekunder
+            Console.Write("Ange låtens längd i minuter: ");
+            int minuter = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange låtens längd i sekunder: ");
+            int sekunder = int.Parse(Console.ReadLine());
+            //Ifall minuter och sekunder är 45 eller mer. Låten kan ha från 2min till 4 min. Sista delen visar att 4 min och 20 sek är max.
+            if ((minuter == 2 && sekunder >= 45) || (minuter > 2 && minuter < 4) || (minuter == 4 && sekunder <= 20))
+            {
+                Console.WriteLine("Låten får spelas på radiostationen.");
+            }
+            // ifall ingen av tiderna som jag lagt kommer det inte gå att spelas.
+            else
+            {
+                Console.WriteLine("Låten får inte spelas på radiostationen.");
+            }
+        }
+    }
+}
